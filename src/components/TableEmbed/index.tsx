00ecf9c39485a7a1a378bc48e97aa1988e422406
Pablo <pablocorrea523@gmail.com>
2022-08-25 18:13:43 -0400

    design tabs added

--- src/components/TableEmbed/index.tsx
+++ src/components/TableEmbed/index.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import { Container } from './styles'
+import React, { useState } from 'react'
+import { Container, Menu, SubTitle } from './styles'
 
 type TableEmbedProps = { 
   lookId: number
 }
 
 const TableEmbed = ({ lookId }: TableEmbedProps) => {
+  const [tabSelected, setTabSelected] = useState(0)
+
   return (
-    <Container>
-      <iframe
+    <>
+      <SubTitle>
+        <h2>Productividad</h2>
+      </SubTitle>
+      <Container>
+        <Menu>
+          <a href="">UF1</a>
+          <a href="">UF2</a>
+          <a onClick={() => setTabSelected(0)} className={`${!tabSelected && 'isSelected'}`}>Rol</a>
+          <a href="">País</a>
+          <a onClick={() => setTabSelected(1)} className={`${tabSelected && 'isSelected'}`}>Población</a>
+        </Menu>
+        <iframe
         src={`https://latamdev.cloud.looker.com/embed/looks/${lookId}?show_title=false`}
         width="100%"
-        height="150px"
+        height="100%"
         frameBorder="0"
-      />
-    </Container>
+        />
+      </Container>
+    </>  
   )
 }
 
