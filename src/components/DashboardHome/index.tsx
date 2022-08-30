d28a89ffe0e5ef70956a6ea2cc25ce39654e2535
Fernando Arriagada López <farriagl@emeal.nttdata.com>
2022-08-30 10:28:43 -0400

    add new dashboard home

--- src/components/DashboardHome/index.tsx
+++ src/components/DashboardHome/index.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Container } from './styles'
 
 const DashboardHome = () => {
-  const width = window.innerWidth - 300
+  const width = window.innerWidth - 284
   return (
     <Container>
       <iframe
-        src="https://latamdev.cloud.looker.com/embed/dashboards/385?show_filters_bar=false&show_title=false"
+        src={`https://latamdev.cloud.looker.com/embed/dashboards/dm_commercial::dashboard_home?filters_bar_collapsed=true`}
+        // src={`https://latamdev.cloud.looker.com/embed/dashboards/385?_theme={"show_title":false}`}
         width={width}
-        height="400"
+        height="510"
         frameBorder="0"
       />
     </Container>
--- src/components/DashboardHome/styles.ts
+++ src/components/DashboardHome/styles.ts
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: #fff;
-  height: 367px;
+  height: calc(510px - 33px);
   overflow: hidden;
   border: 1px solid #EBEBEB;
   box-shadow: 0px 0px 12px 3px rgba(0, 0, 0, 0.08);
   border-radius: 8px;
-  width: calc(100% - 237px);
+  width: 100%;
 `;
