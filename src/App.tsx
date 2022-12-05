72a606151e1b08c48e9a439993544465384b1190
Constanza Celery Calderon <ccelery@CLRTLH3DJ07T4DV.local>
2022-12-05 15:53:00 -0300

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
+import { HelloWorld } from './HelloWorld'
+import { ExtensionProvider } from '@looker/extension-sdk-react'
 import { hot } from 'react-hot-loader/root'
-import Layout from './components/Layout'
-import Home from './views/Home'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import RHConnect from './views/RHConnect'
-import IncomeExits from './views/IncomeExits'
-import RemunerationAccount from './views/RemunerationAccount'
-import { ExtensionProvider2 } from '@looker/extension-sdk-react'
-import { Looker40SDK } from '@looker/sdk'
 
-export const App = hot(() => (
-  <Router>
-    <ExtensionProvider2 type={Looker40SDK}>
-      <Layout>
-        <Switch>
-          <Route exact path="/">
-            <RHConnect />
-          </Route>
-        </Switch>
-      </Layout>
-    </ExtensionProvider2>
-  </Router>
-))
+export const App = hot(() => {
+  return (
+    <ExtensionProvider>
+      <HelloWorld />
+    </ExtensionProvider>
+  )
+})
