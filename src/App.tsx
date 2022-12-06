7c5493df6c0edc93bd422c1b247c0275b2b2049c
Fernando Arriagada López <farriagl@emeal.nttdata.com>
2022-12-06 12:22:23 -0300

    add changes fala

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { hot } from 'react-hot-loader/root'
 import Layout from './components/Layout'
 import Home from './views/Home'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import RHConnect from './views/RHConnect'
-import IncomeExits from './views/IncomeExits'
-import RemunerationAccount from './views/RemunerationAccount'
 import { ExtensionProvider2 } from '@looker/extension-sdk-react'
 import { Looker40SDK } from '@looker/sdk'
 
@@ -15,7 +12,7 @@ export const App = hot(() => (
       <Layout>
         <Switch>
           <Route exact path="/">
-            <RHConnect />
+            <Home />
           </Route>
         </Switch>
       </Layout>
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,26 +1,10 @@
-// import TableProductivity from '../../components/TableProductivity'
 import React from 'react'
-import BoxEmbed from '../../components/BoxEmbed'
-import DashboardHome from '../../components/DashboardHome'
-import Embed from '../../components/Embed'
-import TableEmbed from '../../components/TableEmbed'
-import { Container, Box, BoxTable, Titles } from './styles'
+import { Container } from './styles'
 
 const Home = () => {
   return (
     <Container>
-      <h1>Dotación y Vacantes</h1>
-      <Titles>
-        <h2>Datos clave</h2>
-        <h2>Dotación</h2>
-      </Titles>
-      <Box>
-        <DashboardHome />
-      </Box>
-      <BoxTable>
-        <TableEmbed tableId={1} title={'Productividad'}/>
-        <TableEmbed tableId={2} title={'Vacantes'}/>
-      </BoxTable>
+      <h1>Home</h1>
     </Container>
   )
 }
